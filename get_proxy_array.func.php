bfe82d913e3d4e56ffbb9aa6e9289dfe07dd8930
Dallier <manuel_tersteege@hotmail.com>
2014-09-30 00:25:38 +0200

    Create get_proxy_array.func.php

---
+++ get_proxy_array.func.php
@@ -0,0 +1,9 @@
+<?php
+function get_proxy_array($url){ 
+  $result  = preg_match_all("/\d+\.\d+\.\d+\.\d+\:\d+/", file_get_contents($url), $match);
+    if ($result)
+      foreach ($match[0] as $value)
+        $proxies[] = $value;
+  return array_unique($proxies);
+}
+?>
